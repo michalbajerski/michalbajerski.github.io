5e6113b870dcbc8a212d6df226c588b43f1fa7f9
mbajerski <contact.michalbajerski@gmail.com>
2024-02-22 10:36:26 +0100

    Update reklamacje and kontakt

--- components/cards-kontakt.js
+++ components/cards-kontakt.js
@@ -53,17 +53,31 @@ class CardsKontakt extends HTMLElement {
     </style>
     <div class="cards">
         <div class="card">
-            <h5>Dane siedziby</h5>
-            <p>80-330 Gdańsk, Pilotów 19</p>
+            <h5>Echostar</h5>
+            <p>Poznań, Osiedle Bolesława Chrobrego 26, pawilon nr. 11</p>
+            <p>E-mail: biuro@echostar.pl</p>
+            <p>Tel.: (61) 671 00 00</p>
         </div>
         <div class="card">
-            <h5>Adres e-mail</h5>
-            <p>adresmailowy@gmail.com</p>
+            <h5>VirtuaOperator</h5>
+            <p>00-667 Warszawa,<br>Koszykowa 61B</p>
+            <p>E-mail: biuro@virtuaoperator.pl</p>
+            <p>Tel.: +48 22 10 22 222</p>
         </div>
         <div class="card">
-            <h5>Numer telefonu</h5>
-            <p>+48 123 123 123</p>
+            <h5>FiberX</h5>
+            <p>04-902 Warszawa,<br>ul. Chryzantemy 3</p>
+            <p>E-mail: biuro@fiberx.pl</p>
+            <p>Tel.: +48 22 1022 000</p>
         </div>
+        <div class="card">
+            <h5>ServCom</h5>
+            <p>62-240 Trzemeszno,<br>ul. Foluska 6</p>
+            <p>E-mail: info@serv-net.pl</p>
+            <p>Tel.: 61 669 90 00</p>
+        </div>
+    
+
     </div>
       `;
     }
